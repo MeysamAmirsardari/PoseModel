6b1e1eb088844c95940126086d477b7fcb5287a9
Meysam Amirsardari <62254770+MeysamAmirsardari@users.noreply.github.com>
2021-11-25 16:50:36 +0330

    next step!

---
+++ Evaluation/Evaluation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def MPJPE():
+    #TODO
+    pass
+
+def PCK():
+    #TODO
+    pass
+
+def AP():
+    #TODO
+    pass
+
+def OKS():
+    #TODO
+    pass
---
+++ main.py
@@ -0,0 +1,69 @@
+import click
+
+from csvHandler import csvReader
+
+filePath = ''
+predictedFileName = 'Test1.csv'
+evaluationFileName = 'Eval.csv'
+videoPath = ''
+
+DEFAULT_CONFIG = {
+    'video_extension': 'avi',
+    'converted_video_speed': 1,
+    'calibration': {
+        'animal_calibration': False,
+        'calibration_init': None,
+        'fisheye': False
+    },
+    'manual_verification': {
+        'manually_verify': False
+    },
+    'triangulation': {
+        'ransac': False,
+        'optim': False,
+        'scale_smooth': 2,
+        'scale_length': 2,
+        'scale_length_weak': 1,
+        'reproj_error_threshold': 5,
+        'score_threshold': 0.8,
+        'n_deriv_smooth': 3,
+        'constraints': [],
+        'constraints_weak': []
+    },
+    'pipeline': {
+        'videos_raw': 'videos-raw',
+        'videos_raw_mp4': 'videos-raw-mp4',
+        'pose_2d': 'pose-2d',
+        'pose_2d_filter': 'pose-2d-filtered',
+        'pose_2d_projected': 'pose-2d-proj',
+        'pose_3d': 'pose-3d',
+        'pose_3d_filter': 'pose-3d-filtered',
+        'videos_labeled_2d': 'videos-labeled',
+        'videos_labeled_2d_filter': 'videos-labeled-filtered',
+        'calibration_videos': 'calibration',
+        'calibration_results': 'calibration',
+        'videos_labeled_3d': 'videos-3d',
+        'videos_labeled_3d_filter': 'videos-3d-filtered',
+        'angles': 'angles',
+        'summaries': 'summaries',
+        'videos_combined': 'videos-combined',
+        'videos_compare': 'videos-compare',
+        'videos_2d_projected': 'videos-2d-proj',
+    },
+    'filter': {
+        'enabled': False,
+        'type': 'medfilt',
+        'medfilt': 13,
+        'offset_threshold': 25,
+        'score_threshold': 0.05,
+        'spline': True,
+        'n_back': 5,
+        'multiprocessing': False
+    },
+    'filter3d': {
+        'enabled': False
+    }
+}
+
+lmRows, scorers = csvReader(fileName)
+
